0d0b2a8e688961f339c594e2ec2db7fdc2f400fa
rahulgattu07 <rgattu1@asu.edu>
2022-10-20 22:25:09 -0700

    main function

--- App.java
+++ App.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         Facade f = new Facade();
         Person p = new Person();
         ProduceProductMenu p1 = new ProduceProductMenu();
         MeatProductMenu m1=new MeatProductMenu();
         if(f.login()){
-        p.showMenu();
-        System.out.println("\n");
-        p1.showMenu();
-        m1.showMenu();
+            System.out.println("Bridge pattern implemented");
+            System.out.println("1. meat menu  2. produce menu");
+            System.out.println("enter the menu option");
+            int opt = sc.nextInt();
+            if(opt==1){
+                m1.showMenu();
+
+            }
+            else if (opt==2){
+                p1.showMenu();
+            }
+            else{
+                System.out.println("invalid option");
+            }
+
+
+
+
         }
     }
 }
