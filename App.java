f4c535cc4cfaed01bd76c5efe75f3138a6b6c19c
rahulgattu07 <rgattu1@asu.edu>
2022-10-20 19:30:13 -0700

    main function

--- App.java
+++ App.java
@@ -4,9 +4,11 @@ public class App {
         Person p = new Person();
         ProduceProductMenu p1 = new ProduceProductMenu();
         MeatProductMenu m1=new MeatProductMenu();
+        if(f.login()){
         p.showMenu();
         System.out.println("\n");
         p1.showMenu();
         m1.showMenu();
+        }
     }
 }
