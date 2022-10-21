aa0d5d13e22a47a2ee7d84fdffbf99548fae4df3
rahulgattu07 <rgattu1@asu.edu>
2022-10-20 18:52:55 -0700

    meat Items display

---
+++ MeatProductMenu.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class MeatProductMenu implements ProductMenu {
+
+	public void showMenu() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Rahul Gattu\\Desktop\\DP-zip\\ProductInfo.txt"));
+			String str = br.readLine();
+			while (str!=null){
+				StringTokenizer s = new StringTokenizer(str,":");
+				while(s.hasMoreTokens()){
+					String item = s.nextToken();
+					String value = s.nextToken();
+					if(item.equals("Meat"))
+					{
+						
+
+						System.out.println(item +":"+value);
+					}
+					
+				}
+				
+				str=br.readLine();
+			}			
+		}			
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void showAddButton() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public void showComboxes() {
+
+	}
+
+}
