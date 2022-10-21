c0e53339511174759b2eabb8a19c017c1215a2b8
rahulgattu07 <rgattu1@asu.edu>
2022-10-20 18:24:04 -0700

    main function

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Facade f = new Facade();
+        Person p = new Person();
+        p.showMenu();
+    }
+}
