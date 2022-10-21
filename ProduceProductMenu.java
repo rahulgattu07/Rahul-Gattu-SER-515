50d9b99260bd09b957d2a3bb8d4f0b7d6d5d77ba
rahulgattu07 <rgattu1@asu.edu>
2022-10-20 18:48:30 -0700

    Produce Items display

---
+++ ProduceProductMenu.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class ProduceProductMenu implements ProductMenu {
+
+	public void showMenu() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Rahul Gattu\\Desktop\\DP-zip\\ProductInfo.txt"));
+			String str = br.readLine();
+			while (str!=null){
+				StringTokenizer s = new StringTokenizer(str,":");
+				while(s.hasMoreTokens()){
+					String item = s.nextToken();
+					String value = s.nextToken();
+					if(item.equals("Produce"))
+					{
+						
+
+						System.out.println(item +":"+value);
+					}
+					
+				}
+				
+				str=br.readLine();
+			}			
+		}			
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void showAddButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showComboxes() {
+
+	}
+
+}
