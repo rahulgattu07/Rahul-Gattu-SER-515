db6ba270196f483348c4ef95972230448113462b
rahulgattu07 <rgattu1@asu.edu>
2022-10-20 18:21:01 -0700

    Person items displayed

---
+++ Person.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Person {
+
+	private ProductMenu productMenu;
+
+	public void showMenu() {
+		try {		
+		    FileReader fr = new FileReader(
+            "C:\\Users\\Rahul Gattu\\Desktop\\DP-zip\\UserProduct.txt");
+            int i;
+        
+            while ((i = fr.read()) != -1){
+			System.out.println("Person Cart Items");
+             System.out.print((char)i);
+			}
+		}
+
+			catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	}
+
+	public void showAddButton() {
+
+	}
+
+	public void showViewButton() {
+
+	}
+
+	public void showRadioButton() {
+
+	}
+
+	public void showLabels() {
+
+	}
+
+	public ProductMenu CreateProductMenu() {
+		return null;
+	}
+
+}
