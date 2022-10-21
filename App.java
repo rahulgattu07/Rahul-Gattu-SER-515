89299cbe82debd5fd4977d404ebd1ad4b66cdbc9
rahulgattu07 <rgattu1@asu.edu>
2022-10-20 18:52:02 -0700

    main function

--- App.java
+++ App.java
@@ -3,8 +3,10 @@ public class App {
         Facade f = new Facade();
         Person p = new Person();
         ProduceProductMenu p1 = new ProduceProductMenu();
+        MeatProductMenu m1=new MeatProductMenu();
         p.showMenu();
         System.out.println("\n");
         p1.showMenu();
+        m1.showMenu();
     }
 }
